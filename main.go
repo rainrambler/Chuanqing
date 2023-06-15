978515f464208260214c6d6def24e52b076e2b0a
rainrambler <wanganyu@outlook.com>
2023-06-15 20:53:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,17 +6,7 @@ import (
 
 const KeyDemo = "aabbccdd"
 
-func gcm_demo() {
-	s := EncryptGcm(`今天中午去哪里吃？`, KeyDemo)
-	DecryptGcm(s, KeyDemo)
-}
-
-func ecb_demo() {
-	s := EncryptMsg(`今天中午去哪里吃？`, KeyDemo)
-	fmt.Printf("Enc: %s\n", s)
-	res := DecryptMsg(s, KeyDemo)
-	fmt.Printf("Plain: %s\n", res)
-}
+var convInst CharConv
 
 func ecb_char_demo() {
 	s := EncryptToSongci(`今天中午去哪里吃？`, KeyDemo)
@@ -26,5 +16,6 @@ func ecb_char_demo() {
 }
 
 func main() {
+	convInst.Load(`SongciRank.txt`)
 	ecb_char_demo()
 }
